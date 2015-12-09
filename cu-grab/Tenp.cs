3c1e1f92938f2dd836d92e3884b97240c9966990
Haaglar <ohaag63@gmail.com>
2015-12-09 12:48:41 +1100

    Code shuffle, removed old options

---
+++ cu-grab/Tenp.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Web.Script.Serialization;
+using System.Windows.Controls;
+
+namespace cu_grab
+{
+    public static class Tenp
+    {
+        private static RootObject shows;
+        private static List<Episode> selectedShowEpisodes = new List<Episode>();
+
+        /// <summary>
+        /// Fillthe ListBox with the shows currently on Tenplay found from the search JSON.
+        /// </summary>
+        public static void fillShowsList(ListBox objectList)
+        {
+
+            WebRequest reqSearchJs = HttpWebRequest.Create("http://tenplay.com.au/web%20api/showsearchjson");
+            WebResponse resSearchJs = reqSearchJs.GetResponse();
+
+            using (StreamReader srjs = new StreamReader(resSearchJs.GetResponseStream(), System.Text.Encoding.UTF8))
+            {
+                string jsonjs = srjs.ReadToEnd();
+                JavaScriptSerializer jss = new JavaScriptSerializer();
+                shows = jss.Deserialize<RootObject>(jsonjs);
+                shows.Shows = shows.Shows.OrderBy(x => x.Name).ToList();
+                objectList.ItemsSource = shows.Shows;
+            }
+            resSearchJs.Close();
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="objectList"></param>
+        /// <returns></returns>
+        public static String clickDisplayedShow(ListBox objectList)
+        {
+            WebRequest reqShow = HttpWebRequest.Create("http://tenplay.com.au" + shows.Shows[objectList.SelectedIndex].ShowURL);
+            WebResponse resShow = reqShow.GetResponse();
+
+            StreamReader srShow = new StreamReader(resShow.GetResponseStream(), System.Text.Encoding.UTF8);
+            string pageShow = srShow.ReadToEnd();
+
+            //REGEX
+            Regex idFind = new Regex(@"<div class=""content-card__image-container"">(.+?)</div>", RegexOptions.Singleline); //Finds all the episodes for the show
+            Regex regexIDImage = new Regex(@"(?<=\bsrc="")[^""]*");      //ID from image
+            Regex regexIDEpisode = new Regex(@"(?<=\balt="")[^""]*");      //Episode name from image
+            Regex IDSplit = new Regex(@"([0-9]+)", RegexOptions.RightToLeft); //Get the correct id
+
+            //Get and iterate over the episodes divs
+            foreach (Match matchID in idFind.Matches(pageShow))
+            {
+                //get src ID and episode name
+                MatchCollection matchIdImage = regexIDImage.Matches(matchID.Value);
+                MatchCollection matchIdName = regexIDEpisode.Matches(matchID.Value);
+                if (matchIdImage.Count > 0)
+                {
+                    String valueFull = matchIdImage[0].Value;
+                    String split = valueFull.Split('?')[1];
+                    String final = IDSplit.Matches(split)[0].Value;
+                    selectedShowEpisodes.Add(new Episode(matchIdName[0].Value, final));
+                }
+            }
+            //Store the current show name for file naming later
+            String selectedShow = shows.Shows[objectList.SelectedIndex].Name;
+            //Clean the name for windows
+            foreach (var c in System.IO.Path.GetInvalidFileNameChars())
+            {
+                selectedShow = selectedShow.Replace(c, '-');
+            }
+            resShow.Close();
+            srShow.Close();
+            //Update list and states
+            objectList.ItemsSource = selectedShowEpisodes;
+            return selectedShow;
+            
+        }
+        /// <summary>
+        /// Requests master URL
+        /// </summary>
+        /// <param name="objectList"></param>
+        /// <returns></returns>
+        public static String getUrl(ListBox objectList)
+        {
+            String BC_URL = "http://c.brightcove.com/services/mobile/streaming/index/master.m3u8?videoId="; //url taken from and m3u8
+            String PUB_ID = "&pubId=2199827728001"; //ID taken from any m3u8
+
+            // Get standard m3u8from
+            WebRequest reqm3u8 = HttpWebRequest.Create(BC_URL + selectedShowEpisodes[objectList.SelectedIndex].EpisodeID + PUB_ID);
+            WebResponse resm3u8 = reqm3u8.GetResponse();
+            StreamReader srm3u8 = new StreamReader(resm3u8.GetResponseStream(), System.Text.Encoding.UTF8);
+
+            String url = getHighestm3u8(srm3u8);
+            resm3u8.Close();
+            srm3u8.Close();
+            return url;
+
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="objectList"></param>
+        /// <returns></returns>
+        public static String getSelectedName(ListBox objectList)
+        {
+            return selectedShowEpisodes[objectList.SelectedIndex].Name;
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="objectList"></param>
+        public static void cleanEpisodes(ListBox objectList)
+        {
+            selectedShowEpisodes.Clear();
+            objectList.ItemsSource = shows.Shows;
+        }
+
+        /// <summary>
+        /// Gets the highest rendition from a master m3u8 (move to a general class)
+        /// </summary>
+        /// <param name="m3u8"></param>
+        /// <returns></returns>
+        public static String getHighestm3u8(StreamReader m3u8)
+        {
+            String line; // current line 
+            String finalUrl = "";
+            Regex regexBandwidth = new Regex(@"(?<=\bBANDWIDTH=)([0-9]+)"); //Quality Selection
+            // TODO: Write this section better
+            int index = 0;
+            int row = -1;
+            long bandwidth = 0;
+            long tmp = 0;
+            //Get the highest quality link
+            while ((line = m3u8.ReadLine()) != null)
+            {
+                if (row == index)
+                {
+                    finalUrl = line;
+                }
+                index++;
+                MatchCollection matchBand = regexBandwidth.Matches(line);
+                if (matchBand.Count > 0)
+                {
+                    tmp = int.Parse(matchBand[0].Value);
+                    if (tmp > bandwidth)
+                    {
+                        row = index;
+                        bandwidth = tmp;
+                    }
+                }
+            }
+            return finalUrl;
+
+        }
+    }
+}

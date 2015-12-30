7fac17c594284b37ceef8e69e9589844b62cd022
Haaglar <ohaag63@gmail.com>
2015-12-31 08:59:03 +1100

    Added shell code for upcoming feature.

---
+++ cu-grab/SubtitleConverter.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cu_grab
+{
+    private class Subtitle
+    {
+        List<DateTime> startTime;
+        List<DateTime> endTime;
+        List<String> content;
+        public Subtitle()
+        {
+            startTime = new List<DateTime>();
+            endTime = new List<DateTime>();
+            content = new List<String>();
+        }
+    }
+    public class SubtitleConverter
+    {
+        public SubtitleConverter()
+        {
+
+        }
+        private void dfxpToLocal()
+        {
+
+        }
+        private void localToStr()
+        {
+
+        }
+        public void dfxpToStr()
+        {
+
+        }
+    }
+}

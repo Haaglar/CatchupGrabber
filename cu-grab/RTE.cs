150fa220851ea91f768ab39f49d54cf1c418cf5f
Haaglar <ohaag63@gmail.com>
2016-01-16 22:09:03 +1100

    Display episode list to RTE

--- cu-grab/RTE.cs
+++ cu-grab/RTE.cs
@@ -5,6 +5,7 @@ using System.IO;
 using System.Linq;
 using System.Net;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Web.Script.Serialization;
 using System.Windows.Controls;
@@ -64,10 +65,34 @@ namespace cu_grab
                 }
             }
             //Crop stuff
-            int startPoint = pageShow.IndexOf("main-content-box-container indexpage-content-box"); // Start of episode list
+            int startPoint = pageShow.IndexOf("main-content-box clearfix"); // Start of episode list (two occurances, want first anyway)
             int endPoint = pageShow.IndexOf("main-content-box-container  black");   //recommended rubbish
             pageShow = pageShow.Substring(startPoint, endPoint - startPoint);
-            return pageShow;
+
+            //Regex searches
+            Regex regName = new Regex(@"thumbnail-title"">(.*)<");
+            Regex regID = new Regex(@"href="".*/([0-9].*)/");
+
+            MatchCollection matchName = regName.Matches(pageShow);
+            MatchCollection matchID = regID.Matches(pageShow);
+
+            //Add episodes to list
+            int i = 0;
+            foreach (Match match in matchName)
+            {
+                String ID = matchID[i].Groups[1].Value;
+                String description = match.Groups[1].Value;
+                selectedShowEpisodes.Add(new Episode(description, ID));
+                i++;
+            }
+            String selectedShow = rteShows[objectList.SelectedIndex].v;
+            //Clean the name for windows
+            foreach (var c in System.IO.Path.GetInvalidFileNameChars())
+            {
+                selectedShow = selectedShow.Replace(c, '-');
+            }
+            objectList.ItemsSource = selectedShowEpisodes;
+            return selectedShow;
         }
         public override string getUrl()
         {

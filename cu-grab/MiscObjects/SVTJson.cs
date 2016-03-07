0cfbb77404dabf43604932928cfc35fade048208
Haaglar <ohaag63@gmail.com>
2016-03-07 18:25:58 +1100

    Added New misc object

---
+++ cu-grab/MiscObjects/SVTJson.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cu_grab.MiscObjects
+{
+    public class VideoReference
+    {
+        public string url { get; set; }
+        public int bitrate { get; set; }
+        public string playerType { get; set; }
+    }
+
+    public class SubtitleReference
+    {
+        public string url { get; set; }
+    }
+
+    public class Video
+    {
+        public List<VideoReference> videoReferences { get; set; }
+        public List<SubtitleReference> subtitleReferences { get; set; }
+        public int position { get; set; }
+        public int materialLength { get; set; }
+        public bool live { get; set; }
+        public bool availableOnMobile { get; set; }
+    }
+
+    public class Statistics
+    {
+        public string client { get; set; }
+        public string mmsClientNr { get; set; }
+        public string context { get; set; }
+        public string programId { get; set; }
+        public string mmsCategory { get; set; }
+        public string broadcastDate { get; set; }
+        public string broadcastTime { get; set; }
+        public string category { get; set; }
+        public string statisticsUrl { get; set; }
+        public string title { get; set; }
+        public string folderStructure { get; set; }
+    }
+
+    public class Context
+    {
+        public string title { get; set; }
+        public string programTitle { get; set; }
+        public string thumbnailImage { get; set; }
+        public string posterImage { get; set; }
+        public string popoutUrl { get; set; }
+        public string programVersionId { get; set; }
+    }
+
+    public class SVTJson
+    {
+        public int videoId { get; set; }
+        public Video video { get; set; }
+        public Statistics statistics { get; set; }
+        public Context context { get; set; }
+    }
+}

111e3bd714cf62c7d8f0bb92c84624a09c850144
Haaglar <ohaag63@gmail.com>
2016-04-05 16:46:46 +1000

    Removed some unused usings

--- cu-grab/MainWindow.xaml.cs
+++ cu-grab/MainWindow.xaml.cs
@@ -1,28 +1,9 @@
-﻿using Microsoft.Win32;
-using System;
+﻿using System;
 using System.Collections.Generic;
-using System.Diagnostics;
 using System.IO;
-using System.Linq;
-using System.Net;
-using System.Text;
-using System.Text.RegularExpressions;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-using System.Xml;
-using System.Reflection;
-using System.Web.Script.Serialization;
-using System.ComponentModel;
-using SubCSharp;
-using cu_grab.NetworkAssister;
 namespace cu_grab
 {
     /* Note: Requires FFmpeg

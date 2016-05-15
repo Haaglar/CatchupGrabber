269b27211f1a2ef9b5989b0a1096f76b1cb559ca
Haaglar <ohaag63@gmail.com>
2016-05-15 16:30:53 +1000

    Updated version no and filename

--- cu-grab/Properties/AssemblyInfo.cs
+++ cu-grab/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Windows;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("cu-grab")]
+[assembly: AssemblyTitle("CatchupGrabber")]
 [assembly: AssemblyDescription("Download video from a variety of tv catch-up services.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("cu-grab")]
+[assembly: AssemblyProduct("CatchupGrabber")]
 [assembly: AssemblyCopyright("")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -49,5 +49,5 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.10.0.0")]
-[assembly: AssemblyFileVersion("1.10.0.0")]
+[assembly: AssemblyVersion("2.0.0.0")]
+[assembly: AssemblyFileVersion("2.0.0.0")]

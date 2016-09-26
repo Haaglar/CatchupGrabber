d824441ab131e404277d9fb4de1608b8b30f2a5b
Haaglar <ohaag63@gmail.com>
2016-09-26 11:42:18 +1000

    Livestream rip now working

--- CatchupGrabber/LiveStream.xaml.cs
+++ CatchupGrabber/LiveStream.xaml.cs
@@ -1,16 +1,9 @@
-﻿using System;
+﻿using CatchupGrabber.MiscObjects.LiveStreamSites;
+using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Diagnostics;
+using System.IO;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
 
 namespace CatchupGrabber
 {
@@ -19,9 +12,37 @@ namespace CatchupGrabber
     /// </summary>
     public partial class LiveStream : Window
     {
+        List<LSSites> sites;
         public LiveStream()
         {
             InitializeComponent();
+            sites = new List<LSSites>();
+            sites.Add(new LSSites() { Name = "Channel 9 Australia", URL = "https://9nowch9livehls-i.akamaihd.net/hls/live/250962/melbourne/master1.m3u8", RequiresInitialRequest = false });
+            sites.Add(new LSSites() { Name = "Channel 9 Gem Australia", URL = "https://9nowgemlivehls-i.akamaihd.net/hls/live/250972/melbourne/master1.m3u8", RequiresInitialRequest = false });
+            sites.Add(new LSSites() { Name = "Channel 9 GO Australia", URL = "https://9nowgolivehls-i.akamaihd.net/hls/live/250977/melbourne/master1.m3u8", RequiresInitialRequest = false });
+            sites.Add(new LSSites() { Name = "Channel 9 Life Australia", URL = "https://9nowlifelivehls-i.akamaihd.net/hls/live/250992/melbourne/master1.m3u8", RequiresInitialRequest = false });
+            comboBoxSites.ItemsSource = sites;
+            comboBoxSites.SelectedIndex = 0;
+        }
+
+        private void buttonLogStream_Click(object sender, RoutedEventArgs e)
+        {
+            if (File.Exists("FFmpeg.exe"))
+            {
+                string filename = ((int)DateTime.UtcNow.Subtract(new DateTime(1970, 1, 1)).TotalSeconds) + ".mp4";
+
+                ProcessStartInfo ffmpeg = new ProcessStartInfo();
+                using (Process proc = new Process())
+                {
+                    ffmpeg.FileName = "ffmpeg.exe";
+                    if (Properties.Settings.Default.OverwriteFile)
+                    {
+                        ffmpeg.Arguments = " -i " + sites[comboBoxSites.SelectedIndex].URL + " -acodec copy -vcodec copy -bsf:a aac_adtstoasc " + filename;
+                    }
+                    proc.StartInfo = ffmpeg;
+                    proc.Start();
+                }
+            }
         }
     }
 }
---
+++ CatchupGrabber/MiscObjects/LSSites.cs
@@ -0,0 +1,13 @@
+﻿namespace CatchupGrabber.MiscObjects.LiveStreamSites
+{
+    class LSSites
+    {
+        public string Name { get; set; }
+        public string URL { get; set; }
+        public bool RequiresInitialRequest { get; set; }
+        public override string ToString()
+        {
+            return Name;
+        }
+    }
+}
